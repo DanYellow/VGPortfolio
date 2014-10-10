c77044d23803e1d2a011e617eba1fba8782c5cba
Jean-Louis Danielo <jeanlouis.danielo@sfr.fr>
2014-10-11 00:56:28 +0200

    update gulp fil

---
+++ dev/assets/javascripts/main.js
@@ -0,0 +1,6 @@
+var greetings = require('./foo2');
+
+var foo = new greetings();
+foo.bark();
+
+alert('Hello world !');
--- gulpfile.js
+++ gulpfile.js
@@ -7,57 +7,71 @@ var neat           = require('node-neat').includePaths;
 var mainBowerFiles = require('main-bower-files');
 var connect        = require('gulp-connect');
 
+
+var livereload = require('gulp-livereload');
+
 var browserify = require('gulp-browserify');
 
-gulp.task('connect', function() {
+/*gulp.task('connect', function() {
   connect.server({
     root: 'dev',
     livereload: true,
     port: 3000
   });
-});
+});*/
+
+/*gulp.task('connect', function ()
+{
+    var connect = require('connect');
+    var app = connect()
+        .use(require('connect-livereload')({ port: 35729 }))
+        .use(connect.static('./dev', {default: "app.html"}))
+        .use(connect.static('.tmp'))
+        // paths to bower_components should be relative to the current file
+        // e.g. in app/index.html you should use ../bower_components
+        .use('/bower_components', connect.static('bower_components'))
+        .use(connect.directory('dev'));
+
+    require('http').createServer(app)
+                   .listen(3001)
+                   .on('listening', function () {
+                        console.log('Started connect web server on http://localhost:3001');
+                   });
+});*/
 
 gulp.task('sass', function () {
     gulp.src('dev/assets/stylesheets/**/*.scss')
-        .pipe(sass({
-            includePaths: ['styles'].concat(neat)
-        }))
+        .pipe(sass())
         .pipe(gulp.dest('prod/assets/stylesheets/'))
-        .pipe(connect.reload());
+        .pipe(livereload());
 });
 
 gulp.task('twig', function(){
     gulp.src('dev/views/*.twig')
     .pipe(twig())
     .pipe(gulp.dest('prod'))
-    .pipe(connect.reload());
+    .pipe(livereload());
 });
 
 gulp.task('scripts', function() {
     // Single entry point to browserify
-    gulp.src('dev/assets/javascripts/foo.js')
+    gulp.src('dev/assets/javascripts/main.js')
         .pipe(browserify())
         .pipe(gulp.dest('./prod/assets/javascripts/'))
 });
 
 
-
 gulp.task('images', function () {
   return gulp.src('dev/assets/images/**/*')
     .pipe(gulp.dest('prod/assets/images'))
-    .pipe(connect.reload());
+    .pipe(livereload());
 });
 
-gulp.task('javascripts', function () {
-  return gulp.src(['dev/assets/javascripts/**/*', '!dev/assets/javascripts/old'])
-    .pipe(gulp.dest('prod/assets/javascripts'))
-    .pipe(connect.reload());
-});
 
 gulp.task('watch', function() {
     gulp.watch(['dev/views/**'], ['twig']);
     gulp.watch('bower_components', ['vendors']);
-    gulp.watch(['dev/assets/**'], ['sass', 'images', 'javascripts']);
+    gulp.watch(['dev/assets/**'], ['sass', 'images', 'scripts']);
 });
 
 gulp.task('vendors', function() {
@@ -67,5 +81,5 @@ gulp.task('vendors', function() {
 
 
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['watch']);
 
