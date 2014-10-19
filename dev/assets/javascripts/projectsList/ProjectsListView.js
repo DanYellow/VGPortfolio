23f2153af6d66f9e7c9b98697349e39125acd2f4
Jean-Louis Danielo <jeanlouis.danielo@sfr.fr>
2014-10-19 17:40:40 +0200

    start details page

--- dev/assets/javascripts/projectsList/ProjectsListView.js
+++ dev/assets/javascripts/projectsList/ProjectsListView.js
@@ -1,34 +1,19 @@
 var $ = require('jquery');
 var Backbone = require('backbone');
-var _ = require('underscore');
-
-
 var ProjectsListTpl = require('../tpl/ProjectsListTpl.hbs');
-
-var ProjectsListViewItem = require('../projectsListItem/ProjectsListViewItem');
-
 
 Backbone.$ = $;
 
 
-
 module.exports = Backbone.View.extend({
-  tagName: 'ul',
-	events: {
-   'click' : 'selectedProject'
-  },
-  initialize: function () {
 
-      this.render();
-  },
+    initialize: function () {
+        this.render();
+    },
 
-  render: function () {
-/*      var _that = this;
-      _.each(this.collection.models, function (project)
-      {
-        _that.$el.html(new ProjectsListViewItem({model:project}).render());
-      })*/
+    render: function () {
+        this.$el.html(ProjectsListTpl(this.collection.toJSON()));
 
-      return this;
-  }
+        return this;
+    }
 });
